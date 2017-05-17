354b61ca33e9b11867afa70dc77cdf0a25db2554
Luoxiang <ynztlxdeai@163.com>
2017-05-17 09:55:09 +0800

    fix msgview number show

---
+++ app/src/main/java/com/vincent/template/tablayout/UnreadMsgUtils.java
@@ -0,0 +1,66 @@
+package com.vincent.template.tablayout;
+
+/**
+ * projectName: 	    Template
+ * packageName:	        com.vincent.template.tablayout
+ * className:	        UnreadMsgUtils
+ * author:	            Luoxiang
+ * time:	            2017/5/17	9:53
+ * desc:	            未读消息提示View,显示小红点或者带有数字的红点:
+ *                      数字一位,圆数字两位,圆角矩形,圆角是高度的一半
+ *                      数字超过两位,显示99+
+ *
+ * svnVersion:	        $Rev
+ * upDateAuthor:	    Vincent
+ * upDate:	            2017/5/17
+ * upDateDesc:	        TODO
+ */
+
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class UnreadMsgUtils {
+    public static void show(MsgView msgView, int num) {
+        if (msgView == null) {
+            return;
+        }
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) msgView.getLayoutParams();
+        DisplayMetrics              dm = msgView.getResources().getDisplayMetrics();
+        msgView.setVisibility(View.VISIBLE);
+        if (num <= 0) {//圆点,设置默认宽高
+            msgView.setStrokeWidth(0);
+            msgView.setText("");
+
+            lp.width = (int) (5 * dm.density);
+            lp.height = (int) (5 * dm.density);
+            msgView.setLayoutParams(lp);
+        } else {
+            lp.height = (int) (18 * dm.density);
+            if (num > 0 && num < 10) {//圆
+                lp.width = (int) (18 * dm.density);
+                msgView.setText(num + "");
+            } else if (num > 9 && num < 100) {//圆角矩形,圆角是高度的一半,设置默认padding
+                lp.width = RelativeLayout.LayoutParams.WRAP_CONTENT;
+                msgView.setPadding((int) (6 * dm.density), 0, (int) (6 * dm.density), 0);
+                msgView.setText(num + "");
+            } else {//数字超过两位,显示99+
+                lp.width = RelativeLayout.LayoutParams.WRAP_CONTENT;
+                msgView.setPadding((int) (6 * dm.density), 0, (int) (6 * dm.density), 0);
+                msgView.setText("99+");
+            }
+            msgView.setLayoutParams(lp);
+        }
+    }
+
+    public static void setSize(MsgView rtv, int size) {
+        if (rtv == null) {
+            return;
+        }
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) rtv.getLayoutParams();
+        lp.width = size;
+        lp.height = size;
+        rtv.setLayoutParams(lp);
+    }
+}
+
