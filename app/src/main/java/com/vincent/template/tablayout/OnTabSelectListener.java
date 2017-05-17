8a275037112256d000c90e3a93c3aa7b346244bf
Luoxiang <ynztlxdeai@163.com>
2017-05-17 09:47:41 +0800

    add circle text view for message

---
+++ app/src/main/java/com/vincent/template/tablayout/OnTabSelectListener.java
@@ -0,0 +1,19 @@
+package com.vincent.template.tablayout;
+
+/**
+ * projectName: 	    Template
+ * packageName:	        com.vincent.template.tablayout
+ * className:	        OnTabSelectListener
+ * author:	            Luoxiang
+ * time:	            2017/5/17	9:45
+ * desc:	            TODO
+ *
+ * svnVersion:	        $Rev
+ * upDateAuthor:	    Vincent
+ * upDate:	            2017/5/17
+ * upDateDesc:	        TODO
+ */
+public interface OnTabSelectListener {
+    void onTabSelect(int position);
+    void onTabReselect(int position);
+}
