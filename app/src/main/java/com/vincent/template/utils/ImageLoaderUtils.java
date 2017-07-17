c155d7666e9b8a4622ab79bcd2b8ba4619416f0e
Luoxiang <ynztlxdeai@163.com>
2017-07-17 14:54:39 +0800

    fix template

--- app/src/main/java/com/vincent/template/utils/ImageLoaderUtils.java
+++ app/src/main/java/com/vincent/template/utils/ImageLoaderUtils.java
@@ -3,10 +3,6 @@ package com.vincent.template.utils;
 import android.content.Context;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DecodeFormat;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.jaydenxiao.common.R;
 import com.vincent.template.R;
 
 import java.io.File;
@@ -25,78 +21,104 @@ import java.io.File;
  */
 public class ImageLoaderUtils {
 
-    public static void display(Context context, ImageView imageView, String url, int placeholder, int error) {
+    public static void display(Context context,
+                               ImageView imageView,
+                               String url,
+                               int placeholder,
+                               int error)
+    {
         if (imageView == null) {
             throw new IllegalArgumentException("argument error");
         }
-        Glide.with(context).load(url).placeholder(placeholder)
-                .error(error).crossFade().into(imageView);
+        Glide.with(context)
+             .load(url)
+             .placeholder(placeholder)
+             .error(error)
+             .crossFade()
+             .into(imageView);
     }
 
     public static void display(Context context, ImageView imageView, String url) {
         if (imageView == null) {
             throw new IllegalArgumentException("argument error");
         }
-        Glide.with(context).load(url)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .centerCrop()
-                .placeholder(R.mipmap.ic_image_loading)
-                .error(R.mipmap.ic_empty_picture)
-                .crossFade().into(imageView);
+        Glide.with(context)
+             .load(url)
+             .diskCacheStrategy(DiskCacheStrategy.ALL)
+             .centerCrop()
+             .placeholder(R.mipmap.ic_image_loading)
+             .error(R.mipmap.ic_empty_picture)
+             .crossFade()
+             .into(imageView);
     }
 
     public static void display(Context context, ImageView imageView, File url) {
         if (imageView == null) {
             throw new IllegalArgumentException("argument error");
         }
-        Glide.with(context).load(url)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .centerCrop()
-                .placeholder(R.mipmap.ic_image_loading)
-                .error(R.mipmap.ic_empty_picture)
-                .crossFade().into(imageView);
+        Glide.with(context)
+             .load(url)
+             .diskCacheStrategy(DiskCacheStrategy.ALL)
+             .centerCrop()
+             .placeholder(R.mipmap.ic_image_loading)
+             .error(R.mipmap.ic_empty_picture)
+             .crossFade()
+             .into(imageView);
     }
+
     public static void displaySmallPhoto(Context context, ImageView imageView, String url) {
         if (imageView == null) {
             throw new IllegalArgumentException("argument error");
         }
-        Glide.with(context).load(url).asBitmap()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .placeholder(R.mipmap.ic_image_loading)
-                .error(R.mipmap.ic_empty_picture)
-                .thumbnail(0.5f)
-                .into(imageView);
+        Glide.with(context)
+             .load(url)
+             .asBitmap()
+             .diskCacheStrategy(DiskCacheStrategy.ALL)
+             .placeholder(R.mipmap.ic_image_loading)
+             .error(R.mipmap.ic_empty_picture)
+             .thumbnail(0.5f)
+             .into(imageView);
     }
+
     public static void displayBigPhoto(Context context, ImageView imageView, String url) {
         if (imageView == null) {
             throw new IllegalArgumentException("argument error");
         }
-        Glide.with(context).load(url).asBitmap()
-                .format(DecodeFormat.PREFER_ARGB_8888)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .placeholder(R.mipmap.ic_image_loading)
-                .error(R.mipmap.ic_empty_picture)
-                .into(imageView);
+        Glide.with(context)
+             .load(url)
+             .asBitmap()
+             .format(DecodeFormat.PREFER_ARGB_8888)
+             .diskCacheStrategy(DiskCacheStrategy.ALL)
+             .placeholder(R.mipmap.ic_image_loading)
+             .error(R.mipmap.ic_empty_picture)
+             .into(imageView);
     }
+
     public static void display(Context context, ImageView imageView, int url) {
         if (imageView == null) {
             throw new IllegalArgumentException("argument error");
         }
-        Glide.with(context).load(url)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .centerCrop()
-                .placeholder(R.mipmap.ic_image_loading)
-                .error(R.mipmap.ic_empty_picture)
-                .crossFade().into(imageView);
+        Glide.with(context)
+             .load(url)
+             .diskCacheStrategy(DiskCacheStrategy.ALL)
+             .centerCrop()
+             .placeholder(R.mipmap.ic_image_loading)
+             .error(R.mipmap.ic_empty_picture)
+             .crossFade()
+             .into(imageView);
     }
-    public static void displayRound(Context context,ImageView imageView, String url) {
+
+    public static void displayRound(Context context, ImageView imageView, String url) {
         if (imageView == null) {
             throw new IllegalArgumentException("argument error");
         }
-        Glide.with(context).load(url)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .error(R.mipmap.toux2)
-                .centerCrop().transform(new GlideRoundTransformUtil(context)).into(imageView);
+        Glide.with(context)
+             .load(url)
+             .diskCacheStrategy(DiskCacheStrategy.ALL)
+             .error(R.mipmap.toux2)
+             .centerCrop()
+             .transform(new GlideRoundTransformUtil(context))
+             .into(imageView);
     }
 
 }
